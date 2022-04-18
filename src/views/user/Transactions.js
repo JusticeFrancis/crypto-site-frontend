a82526c6ebb778e2d08a09efd5c8c24004b5afc9
justicefrancis <cjfrancisf@gmail.com>
2022-04-18 13:44:05 +0100

    jd

--- src/views/user/Transactions.js
+++ src/views/user/Transactions.js
@@ -173,7 +173,7 @@ export  default function Transactions(props) {
 { step === 0
     ?(
         <div className="relative flex flex-col min-w-0 break-words w-full mb-6  rounded-lg  border-0" >
-        <div className="px-4 md:px-10 mx-auto w-full">
+        <div className=" w-full">
         <div>
             {/* Card stats */}
             <div className="flex flex-wrap">
