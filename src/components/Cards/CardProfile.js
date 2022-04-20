13d19fa1ac56c879d09d1829ce4d5b9608d79579
justicefrancis <cjfrancisf@gmail.com>
2022-04-20 13:21:20 +0100

    ye

--- src/components/Cards/CardProfile.js
+++ src/components/Cards/CardProfile.js
@@ -7,6 +7,8 @@ export default function CardProfile(props) {
   const [walletType , setWalletType] = useState(0)
   //0 for main, 1 for arbit
 
+  const b = wallet.balanceBTC.toString()
+  const btcBalance = b.substring(0,3)
   const SwitchWallet = ()=>{
       if(walletType === 0){
         setWalletType(1)
@@ -66,7 +68,7 @@ export default function CardProfile(props) {
               </div>
               <div className="lg:mr-4 p-3 text-center">
                 <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
-                {wallet.balanceBTC}
+                {btcBalance}
                 </span>
                 <span className="text-sm text-blueGray-400">BTC</span>
               </div>
