0ce36a9ef153aac67bc0f07ee6dddb46e9e66c6a
justicefrancis <cjfrancisf@gmail.com>
2022-04-27 16:22:27 +0100

    new

--- src/views/admin/Daily.js
+++ src/views/admin/Daily.js
@@ -4,6 +4,7 @@ const Daily = (props) => {
     const {backend} = props
     const [loader , setLoader] = useState(false)
     const [msg , setMsg] = useState('')
+    const [button, setButton] = useState(true)
 
 
 
@@ -64,12 +65,25 @@ const Daily = (props) => {
                         
                           
 
+                          {button 
+                          ?(
                             <button id="myBtn" onClick={()=>{daily()}}
-                                    className="bg-lightBlue-500     text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline mr-1 ease-linear transition-all duration-150"
-                                    type="button"
-                                    >
-                                    Make daily update
-                                </button>
+                            className="bg-lightBlue-500     text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline mr-1 ease-linear transition-all duration-150"
+                            type="button"
+                            >
+                            Make daily update
+                        </button>
+                          )
+                          : (
+                            <button id="myBtn" disabled
+                            className="bg-lightBlue-500     text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline mr-1 ease-linear transition-all duration-150"
+                            type="button"
+                            >
+                            Pls come back tomorrow to update wallet again
+                        </button>
+                          )
+
+                          }
 
                                 
                             </div>
